519c2c062b67b16f4a0db50ea55f770942df5bd9
Sebastian Bekkvik Aas <35501347+sebasa18@users.noreply.github.com>
2026-01-21 15:19:26 +0100

    FEAT 4: rewrite jooq to jdbc (#1)
    * FEAT 4: add SpeciesEntity class
    
    * FEAT 4: add relevant dependencies
    
    * FEAT 4: reword dao stub
    
    ---------
    
    Co-authored-by: Sebastian Bekkvik Aas <sebastian.bekkvik.aas@dnb.no>

---
+++ src/main/java/com/sba/eggcalc/database/SpeciesEntity.java
@@ -0,0 +1,43 @@
+package com.sba.eggcalc.database;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "SPECIES")
+public class SpeciesEntity {
+
+  @Id
+  @Column("monId")
+  @NotNull
+  private Long monId;
+
+  @Column("name")
+  @NotBlank
+  private String name;
+
+  @Column("canBeFemale")
+  @NotNull
+  private Boolean canBeFemale;
+
+  @Column("canBeMale")
+  @NotNull
+  private Boolean canBeMale;
+
+  @Column("group1Id")
+  @NotNull
+  private Integer group1Id;
+
+  @Column("group2Id")
+  private Integer group2Id;
+}
+
+// Whether additional Entities are needed is under FEAT 3: map out needed dao functions.
