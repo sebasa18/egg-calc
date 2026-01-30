a5bdd715f2e1dff2b16afc71211a36ebc543f44b
Sebastian Bekkvik Aas <sebastian.bekkvik.aas@dnb.no>
2026-01-30 08:32:44 +0100

    FEAT 7: add implementation draft

--- src/main/java/com/sba/eggcalc/backend/EggMoveCalc.java
+++ src/main/java/com/sba/eggcalc/backend/EggMoveCalc.java
@@ -1,6 +1,15 @@
 package com.sba.eggcalc.backend;
 
+import com.sba.eggcalc.database.SpeciesEntity;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+import java.util.HashSet;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class EggMoveCalc {
 
@@ -10,34 +19,34 @@ public class EggMoveCalc {
     this.eggMoveDao = eggMoveDao;
   }
 
-  List<String> findEggMoveChain(Integer speciesId, Integer moveId) {
+  List<Integer> findEggMoveChain(Integer speciesId, Integer moveId) {
 
-    List<Integer> origins = eggMoveDao.getLearnsetMons(moveId);
-    List<Integer> visited = List.of();
+    if (speciesId == 9999 || speciesId == 10000) return List.of(); // Manaphy/ditto check
 
-    for (Integer origin : origins) {
-      List<Integer> compatibles = eggMoveDao.getCompatibleMons(speciesId);
-      if (compatibles.contains(origin)) {
-        // found a valid origin
+    boolean hasLevelMove = !eggMoveDao.eggMoveByMonIdAndMoveId(speciesId, moveId);
+    if (hasLevelMove) return List.of(speciesId); // Target has move as level-up move
+
+    SpeciesEntity goalSpeciesEntity = eggMoveDao.getSpeciesByMonId(speciesId);
+    Integer goalGroup1 = goalSpeciesEntity.getGroup1Id();
+
+    if (goalGroup1 == 0) {
+      // NED check
+      return List.of();
+    }
+
+    List<Integer> origins = eggMoveDao.getSpeciesWithLevelMove(moveId);
+
+    DefaultMutableTreeNode top = new DefaultMutableTreeNode(speciesId);
+    JTree visited = new JTree();
+
+    while (true) {
+      HashSet<Integer> rename = new HashSet<>();
+      HashSet<Integer> temp = new HashSet<>();
+      for (Integer currentSpeciesId : origins) {
+        temp.addAll(eggMoveDao.getCompatibleSpecies(currentSpeciesId));
+        temp.retainAll(eggMoveDao.getSpeciesWithEggMove(currentSpeciesId));
+        rename.addAll(temp);
       }
     }
-
-    return List.of("Move1", "Move2", "Move3");
-  }
-
-  private boolean searchChain(Integer currentMon, Integer targetMon, List<Integer> visited) {
-    if (currentMon.equals(targetMon)) {
-      return true;
-    }
-    visited.add(currentMon);
-    List<Integer> compatibles = eggMoveDao.getCompatibleMons(currentMon);
-    for (Integer nextMon : compatibles) {
-      if (!visited.contains(nextMon)) {
-        if (searchChain(nextMon, targetMon, visited)) {
-          return true;
-        }
-      }
-    }
-    return false;
   }
 }
