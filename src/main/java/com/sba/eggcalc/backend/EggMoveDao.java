4ddba43af34ab2ee020a79c980d337416ea6140f
Sebastian Bekkvik Aas <sebastian.bekkvik.aas@dnb.no>
2026-01-30 08:28:16 +0100

    FEAT 7: add rewrite notes

--- src/main/java/com/sba/eggcalc/backend/EggMoveDao.java
+++ src/main/java/com/sba/eggcalc/backend/EggMoveDao.java
@@ -2,8 +2,6 @@ package com.sba.eggcalc.backend;
 
 import com.sba.eggcalc.database.SpeciesEntity;
 import org.jetbrains.annotations.NotNull;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,9 +16,9 @@ import java.util.List;
 @Transactional(propagation = Propagation.MANDATORY)
 public interface EggMoveDao extends CrudRepository<SpeciesEntity, Integer> {
 
-  Integer getMonIdByName(@NotBlank String speciesName);
+  Integer getMonIdByName(@NotNull String speciesName);
 
-  Integer getMoveIdByName(@NotBlank String moveName);
+  Integer getMoveIdByName(@NotNull String moveName);
 
   @NotNull
   @Query("SELECT moveId FROM LEARNSETS WHERE monId = :speciesId AND eggMove = TRUE")
@@ -37,5 +35,22 @@ public interface EggMoveDao extends CrudRepository<SpeciesEntity, Integer> {
   @Query("SELECT s2.monId FROM SPECIES s1 INNER JOIN SPECIES s2 ON s1.monId = :speciesId AND " +
           "(s1.group1Id = s2.group1Id OR s1.group1Id = s2.group2Id OR s1.group2Id = s2.group1Id OR s1.group2Id = s2.group2Id)")
   List<Integer> getCompatibleSpecies(@NotNull Integer speciesId);
+
+  SpeciesEntity getSpeciesByMonId(@NotNull Integer monId);
+
+  Boolean eggMoveByMonIdAndMoveId(@NotNull Integer monId, @NotNull Integer moveId);
+
+  // Redo all methods to get species going outwards instead of inwards, looking for ones sharing exactly one egg group  // At least one is probably fine so long as I filter out dupes
+  // Given a mon and a move, gets everyone that shares at least one egg group with the mon and has the move as an egg move
+  //
+
+  // get mon details given id
+  // Could still need single-group mons if the chain between two groups can't breed with target mon
+  // Don't see this happening outside the target's group though
+
+  // Preemptive checks
+  // If the target mon has the move as a level-up move, return empty list
+  // Target is not manaphy, ditto or an NED
+  // If target is in Field grup, return smeargle
 
 }
